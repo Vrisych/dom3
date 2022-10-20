73b86bd9eef22a94598f5c746f022000b7ef9812
Irina <lebeirin@yandex.ru>
2022-10-20 19:36:22 +0500

    finish

---
+++ 1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначое число и я покажу второе число");
+string num = Console.ReadLine();
+if (num.Length == 3) Console.WriteLine($"{num[1]}");
+else Console.WriteLine("Число не трехзначное");
---
+++ 3/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите целое число от 1 до 7, соответствующий дню недели");
+int num = int.Parse(Console.ReadLine());
+if (num >= 1 && num <= 5) Console.WriteLine("Сегодня будний день");
+else if (num == 6 || num == 7) Console.WriteLine("Сегодня выходной");
+else Console.WriteLine("Число не попало в промежуток");
